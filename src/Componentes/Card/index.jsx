acc7a7c284bf0701efa4a8158199a45585517637
marlon <marlon.mosquerar@gmail.com>
2023-08-05 16:01:19 -0500

    se consumio la Api

--- src/Componentes/Card/index.jsx
+++ src/Componentes/Card/index.jsx
@@ -1,14 +1,14 @@
-function Card () {
+function Card ({data}) {
     return(
-        <div className='bg-white cursor-pointer w-56 h-60'>
+        <div className='bg-white cursor-pointer w-56 h-80'>
             <figure className='relative mb-2 w-full h-4/5'>
-                <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xm m-2 p-1'>Electricos</span>
-                <img className='w-full h-full object-cover rounded-lg' src="https://images.pexels.com/photos/10902946/pexels-photo-10902946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt='celulares'/>
+                <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xm m-2 p-1'>{data.catagory}</span>
+                <img className='w-full h-full object-cover rounded-lg' src={data.image} alt='celulares'/>
                 <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2'>+</div>
             </figure>
             <p className='flex justify-between'>
-                <span className='text-sm font-light'>celulares</span>
-                <span className='text-sm font-light'>$ 500</span>
+                <span className='text-sm font-light'>{data.title}</span>
+                <span className='text-sm font-light'>${data.price}</span>
             </p>
         </div>
     );
--- src/Paginas/Home/index.jsx
+++ src/Paginas/Home/index.jsx
@@ -1,10 +1,38 @@
+import React, { useState,useEffect } from 'react';
 import { Card } from '../../Componentes/Card';
-import {Layout} from '../../Componentes/Layout'
+import {Layout} from '../../Componentes/Layout';
+
+function useApi(api){ // costom hook
+  const [dataApi,setDataApi] = React.useState(null)
+
+  // para consumir una api siempre se usa useEffect
+  useEffect(() =>{
+    fetch(api)
+    .then(respose => respose.json())
+    .then(data => setDataApi(data))
+
+  },[])
+  return(
+    {dataApi}
+  )
+}
+
 function Home() {
+  const {dataApi} = useApi('https://fakestoreapi.com/products')
+  console.log(dataApi)
 return (
     <Layout className='bg-yellow-300'>
       Home
-     <Card/>
+      <div className='grid gap-8 grid-cols-4 w-full max-w-screen-lg '>
+      {
+        dataApi?.map((product) =>(  // Nota: ? verifica si dataApi existe osea diferente a null o undefined para poder renderizar
+           <Card 
+            key={product.id}
+            data = {product}
+           />
+        )) 
+      }
+      </div>
     </Layout>
     
   )
