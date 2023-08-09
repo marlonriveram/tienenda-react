a8b984b4820b65deb40170c8d815c1d05a2d48d3
marlon <marlon.mosquerar@gmail.com>
2023-08-09 15:44:09 -0500

    mostrar producto selecionado en aside Detalle de producto

--- src/Componentes/Card/index.jsx
+++ src/Componentes/Card/index.jsx
@@ -3,16 +3,25 @@ import { IoIosAddCircle } from "react-icons/io";
 import React,{ useContext } from 'react';
 
 function Card ({data}) {
-    const counter = React.useContext(tiendaContext);
+    const contexto = React.useContext(tiendaContext);
+ 
+    const mostrarProducto = (producto) =>{
+        contexto.mostrarDetalleProducto()
+        contexto. setMostrarProducto(producto)
+    };
     return(
-        <div className='bg-white cursor-pointer w-40 h-80'>
+        <div 
+        className='bg-white cursor-pointer w-40 h-80'
+        >
             <figure className='relative mb-2 w-full h-4/5'>
                 <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xm m-2 p-1'>{data.catagory}</span>
-                <img className='w-full h-full object-cover rounded-lg' src={data.image} alt='celulares'/>
-                <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2'
-                onClick={() => counter.setContador(counter.contador + 1) }
+                <img 
+                  onClick={() => mostrarProducto (data)}
+                className='w-full h-full object-cover rounded-lg' src={data.image} alt='celulares'/>
+                <div className='absolute top-0 right-0 flex justify-center items-center bg-transparent w-6 h-6 rounded-full m-2'
+                onClick={() => contexto.setContador(contexto.contador + 1) }
                 >
-                    < IoIosAddCircle/>
+                    < IoIosAddCircle color='blue' size='2rem' />
                 </div>
             </figure>
             <p className='flex justify-between'>
--- src/Paginas/Home/index.jsx
+++ src/Paginas/Home/index.jsx
@@ -14,12 +14,13 @@ function Home() {
 return (
     <Layout className='bg-yellow-300'>
       Home
-      <div className='grid gap-3 grid-cols-4 w-full max-w-screen-lg '>
+      <div className='grid gap-3 grid-cols-4 justify-center w-full max-w-screen-lg '>
       {
         dataApi?.map((product) =>(  // Nota: ? verifica si dataApi existe osea diferente a null o undefined para poder renderizar
            <Card 
             key={product.id}
             data = {product}
+            
            />
         )) 
       }
